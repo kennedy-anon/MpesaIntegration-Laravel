04c07cac75f1e907170dd3319eb82d31936f2e24
kennedy-anon <85305895+kennedy-anon@users.noreply.github.com>
2023-06-06 09:24:58 +0300

    database design

---
+++ database/migrations/2023_06_06_061137_create_payment_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('payment', function (Blueprint $table) {
+            $table->id();
+            $table->integer('sender');
+            $table->decimal('amount', 8, 2);
+            $table->date('date');
+            $table->string('receipt_number');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('payment');
+    }
+};

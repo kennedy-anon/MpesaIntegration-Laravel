1286f14c5d1b0acc03c0e45938e1a28a27c3710d
kennedy-anon <85305895+kennedy-anon@users.noreply.github.com>
2023-06-06 12:12:10 +0300

    styling table

--- resources/views/payments/payed.blade.php
+++ resources/views/payments/payed.blade.php
@@ -1,24 +1,24 @@
 <x-layout>
-    <div class="mx-4">
-        
+    <div class="mx-4 relative overflow-x-auto rounded-lg shadow-md">
+
         @unless (count($payments) == 0)
-        <table class="table-auto">
-            <thead>
+        <table class="table-auto w-full text-sm text-left text-gray-500 dark:text-gray-400">
+            <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
               <tr>
-                <th>Sender</th>
-                <th>Amount</th>
-                <th>Date</th>
-                <th>MpesaReceiptNumber</th>
+                <th scope="col" class="px-6 py-3">Sender</th>
+                <th scope="col" class="px-6 py-3">Amount</th>
+                <th scope="col" class="px-6 py-3">Date</th>
+                <th scope="col" class="px-6 py-3">Mpesa Receipt Number</th>
               </tr>
             </thead>
 
             <tbody>
                 @foreach ($payments as $payment)
-                <tr>
-                    <td>{{ $payment['sender'] }}</td>
-                    <td>{{ $payment['amount'] }}</td>
-                    <td>{{ $payment['date'] }}</td>
-                    <td>{{ $payment['receipt_number'] }}</td>
+                <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                    <td class="px-6 py-4">{{ $payment['sender'] }}</td>
+                    <td class="px-6 py-4">{{ $payment['amount'] }}</td>
+                    <td class="px-6 py-4">{{ $payment['date'] }}</td>
+                    <td class="px-6 py-4">{{ $payment['receipt_number'] }}</td>
                 </tr>
                 @endforeach
 
